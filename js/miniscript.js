4d361c12fa8e0a9351901b3e6a5c61bac94d716f
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2019-10-18 20:50:37 +0300

    исправляем ошибки по критериям

---
+++ js/miniscript.js
@@ -0,0 +1,4 @@
+var section=document.querySelector(".find-hotel-sedona")
+var find=document.querySelector(".find-title");var popup=section.querySelector("form");var hotel=document.querySelector(".find-hotel-form");var input=section.querySelectorAll("input[type=text]");var button=section.querySelector(".find")
+document.addEventListener("DOMContentLoaded",function(){if(typeof(popup)!="underfined"&&popup!=null){popup.classList.remove("js-not")}});if(typeof(find)!="underfined"&&find!=null){find.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("show-must-go-on")})}
+button.addEventListener("click",function(evt){if(!input.value){hotel.classList.add("error")}else{if(isStorageSupport){localStorage.setItem("input",input.value)}}})
